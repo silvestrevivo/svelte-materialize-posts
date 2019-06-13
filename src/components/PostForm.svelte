df8f3f885114b6ca9983bafaf4503a6b51ee801e
silvestrevivo <silvestrevivomillan@gmail.com>
2019-06-13 23:40:18 +0200

    edit and removing posts

--- src/components/PostForm.svelte
+++ src/components/PostForm.svelte
@@ -2,8 +2,10 @@
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
 
-  let title = "";
-  let body = "";
+  export let editingPost;
+
+  $: title = editingPost.title;
+  $: body = editingPost.body;
   let loading = false;
 
   const apiBaseUrl =
@@ -17,15 +19,23 @@
 
     loading = true;
 
+    let url, method;
+
+    if (editingPost.id) {
+      url = `${apiBaseUrl}/post/${editingPost.id}`;
+      method = "PUT";
+    } else {
+      url = `${apiBaseUrl}/post`;
+      method = "POST";
+    }
+
     const newPost = { title, body };
-    const res = await fetch(`${apiBaseUrl}/post`, {
-      method: "POST",
+    const res = await fetch(url, {
+      method,
       body: JSON.stringify(newPost)
     });
     const post = await res.json();
 
-    title = "";
-    body = "";
     loading = false;
     dispatch("postCreated", post);
   }
@@ -42,13 +52,15 @@
     <form on:submit={onSubmit}>
       <div class="input-field">
         <label for="title">Title</label>
-        <input type="text" id="title" bind:value={title} />
+        <input type="text" id="title" bind:value={editingPost.title} />
       </div>
       <div class="input-field">
         <label for="body">Body</label>
-        <input type="text" id="body" bind:value={body} />
+        <input type="text" id="body" bind:value={editingPost.body} />
       </div>
-      <button type="submit" class="waves-effect waves-light btn">Submit</button>
+      <button type="submit" class="waves-effect waves-light btn">
+         {editingPost.id ? 'Update' : 'Add'}
+      </button>
     </form>
   {:else}
     <div class="preloader-wrapper small active">
